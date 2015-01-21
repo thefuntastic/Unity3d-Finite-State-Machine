a99bd85e1d15aca3318e919ea7de641394faab8d
Peter Cardwell-Gardner <peter@thefuntastic.com>
2015-01-21 13:30:28 +0200

    Added failing integration test for changing state from outside the state machine convention

---
+++ example_project/Assets/TestCases/ClassChangeDuringMonoUpdate.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using MonsterLove.StateMachine;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClassChangeDuringMonoUpdate : StateMachineBehaviour
+{
+	public enum States
+	{
+		NotUsed,
+		AlsoNotUsed,
+		One,
+		Two
+	}
+
+	public float oneDuration = 1f;
+
+	private float oneStartTime;
+
+	public int oneEnter = 0;
+	public int twoEnter = 0;
+
+	
+
+	void Awake()
+	{
+			
+		Initialize<States>();
+		
+
+		ChangeState(States.One);
+	}
+	
+	//Use timer here in stead of couroutines to prevent the stack depth getting too deeps, as these couroutines will cycle into each other
+	void One_Enter()
+	{
+		Debug.Log("One: Entered " + Time.time);
+
+		oneStartTime = Time.time;
+
+		oneEnter++;
+	}
+
+
+	//Use the mono behaviour update function to change our states. 
+	void Update()
+	{
+		var state = (States) GetState();
+
+		if(state == States.One)
+		{
+			if (Time.time - oneStartTime > oneDuration)
+			{
+				Debug.Log("Changing to Two : " + Time.time);
+				ChangeState(States.Two);
+			}
+		}
+	}
+
+
+	IEnumerator Two_Enter()
+	{
+		Debug.Log("Two Entered " + Time.time  );
+		twoEnter++;
+
+		yield return null;
+
+	}
+
+
+
+	
+}
