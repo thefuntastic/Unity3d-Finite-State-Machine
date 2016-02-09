491edad199239f5845434914ab3f040d0c90f189
Peter Cardwell-Gardner <peter@thefuntastic.com>
2016-02-09 11:32:06 +0200

    Created test for OnCollisionEnter

---
+++ example_project/Assets/TestCases/ClassDetectOnCollisionEnter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MonsterLove.StateMachine;
+
+public class ClassDetectOnCollisionEnter : StateBehaviour
+{
+	public enum States
+	{
+		One,
+		Two,
+	}
+
+	public bool oneHasCollision = false;
+	public bool twoHasCollision = false;
+
+	void OnCollisionEnter(Collision collision)
+	{
+		Debug.Log(collision.collider);
+	}
+
+	void One_OnCollisionEnter(Collision collision)
+	{
+		oneHasCollision = true;
+		Debug.Log("one has on colliision " + collision.collider);
+	}
+}
