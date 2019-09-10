a372c4dc6bc4ae98d77f0c330ae40e5729a50e31
Peter Cardwell-Gardner <peter@madewithmonsterlove.com>
2019-09-10 11:25:32 +0100

    Moved State Mapping to it's own file

--- example_project/Assets/MonsterLove/StateMachine/StateMachine.cs
+++ example_project/Assets/MonsterLove/StateMachine/StateMachine.cs
@@ -557,44 +557,4 @@ namespace MonsterLove.StateMachine
 			return engine.Initialize<TState>(component, startState);
 		}
 	}
-
-	public class StateMapping<TState, TDriver> where TState : struct, IConvertible, IComparable where TDriver : class, new()
-	{
-		public TState state;
-		public TDriver driver;
-
-		public bool hasEnterRoutine;
-		public Action EnterCall = StateMachineRunner.DoNothing;
-		public Func<IEnumerator> EnterRoutine = StateMachineRunner.DoNothingCoroutine;
-
-		public bool hasExitRoutine;
-		public Action ExitCall = StateMachineRunner.DoNothing;
-		public Func<IEnumerator> ExitRoutine = StateMachineRunner.DoNothingCoroutine;
-
-		public Action Finally = StateMachineRunner.DoNothing;
-
-		private StateMachine<TState, TDriver> fsm;
-
-		public StateMapping(StateMachine<TState, TDriver> fsm, TState state)
-		{
-			this.fsm = fsm;
-			this.state = state;
-			driver = new TDriver();
-		}
-
-		public bool TestInvokable()
-		{
-			if (fsm.IsInTransition)
-			{
-				return false;
-			}
-
-			if (!fsm.IsCurrentState(this))
-			{
-				return false;
-			}
-
-			return true;
-		}
-	}
 }
